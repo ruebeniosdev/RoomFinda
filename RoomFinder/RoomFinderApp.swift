f1daf2fd0446b9bd2eb02c9be760fde6a6be25da
Rueben <akankobatengr@gmail.com>
2025-01-09 17:55:31 +0000

    Initial Commit

---
+++ RoomFinder/RoomFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomFinderApp.swift
+//  RoomFinder
+//
+//  Created by Rueben on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RoomFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
